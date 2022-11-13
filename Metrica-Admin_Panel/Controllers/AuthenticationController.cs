f78799a837700324c79e15debe66121f21994549
beta64y <106177539+beta64y@users.noreply.github.com>
2022-11-13 22:56:20 +0300

    Authentication added

---
+++ Metrica-Admin_Panel/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Metrica_Admin_Panel.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult RePassword()
+        {
+            return View();
+        }
+
+        public IActionResult LockScreen()
+        {
+            return View();
+        }
+
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
